3b53dc8580b1f6a317e857f5e0320dbda202b15f
Zia Ahmed <ziaahmedf17@gmail.com>
2025-04-14 04:48:03 +0500

    Update ContentView.swift
    Added Buttons

--- GuessFlag/ContentView.swift
+++ GuessFlag/ContentView.swift
@@ -9,27 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack
-        {
-            HStack{
-                Text("1")
-                Text("2")
-                Text("3")
-            }
-            HStack{
-                Text("4")
-                Text("5")
-                Text("6")
-            }
-            HStack{
-                Text("7")
-                Text("8")
-                Text("9")
-            }
+        VStack {
+            Button("Button 1"){}
+                .buttonStyle(.bordered)
+            
+            Button("Button 2", role: .destructive) { } .buttonStyle(. bordered)
+            
+            Button ("Button 3") { }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            
+            Button ("Button 4", role: .destructive){}
+                .buttonStyle(.borderedProminent)
         }
     }
 }
-
 #Preview {
     ContentView()
 }
