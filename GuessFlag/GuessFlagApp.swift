465fbebd8d54b5aa4147fc2d59598b66356910ce
Zia Ahmed <ziaahmedf17@gmail.com>
2025-04-14 04:09:10 +0500

    Initial Commit

---
+++ GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by Zi on 14/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuessFlagTests/GuessFlagTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagTests.swift
+//  GuessFlagTests
+//
+//  Created by Zi on 14/04/2025.
+//
+
+import Testing
+@testable import GuessFlag
+
+struct GuessFlagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
