fab925b136459c40094e65081df259e291d0cc2c
Zia Ahmed <ziaahmedf17@gmail.com>
2025-04-14 04:21:35 +0500

    Update ContentView.swift
    Added Stacks

--- GuessFlag/ContentView.swift
+++ GuessFlag/ContentView.swift
@@ -9,14 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("Hello, world!")
+        VStack
+        {
+            HStack{
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            HStack{
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            HStack{
+                Text("7")
+                Text("8")
+                Text("9")
+            }
         }
-        .padding()
     }
 }
 
